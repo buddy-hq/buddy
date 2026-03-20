758e7f063bbc011c594d927e0c3f3585af7b16d8
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2026-03-20 18:08:03 +0530

    feat(opencode-adapter): Export theme module

---
+++ packages/opencode-adapter/src/theme.ts
@@ -0,0 +1,37 @@
+export type {
+  DesktopTheme,
+  ThemePaletteColors,
+  ThemeSeedColors,
+  ThemeVariant,
+  HexColor,
+  OklchColor,
+  ResolvedTheme,
+  ColorValue,
+  CssVarRef,
+  TokenCategory,
+  ThemeToken,
+} from "../../../vendor/opencode/packages/ui/src/theme/types"
+
+export {
+  hexToRgb,
+  rgbToHex,
+  hexToOklch,
+  oklchToHex,
+  rgbToOklch,
+  oklchToRgb,
+  generateScale,
+  generateNeutralScale,
+  generateAlphaScale,
+  fitOklch,
+  blend,
+  mixColors,
+  shift,
+  lighten,
+  darken,
+  withAlpha,
+} from "../../../vendor/opencode/packages/ui/src/theme/color"
+
+export { resolveThemeVariant, resolveTheme, themeToCss } from "../../../vendor/opencode/packages/ui/src/theme/resolve"
+
+export * from "../../../vendor/opencode/packages/ui/src/theme/default-themes"
+export { DEFAULT_THEMES as defaultThemes } from "../../../vendor/opencode/packages/ui/src/theme/default-themes"
