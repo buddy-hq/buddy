8934419118aa810a97ec0fcfe90c38deba63f0ce
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2026-04-17 22:46:06 +0530

    chore(web): Update TanStack dependencies

--- packages/web/src/routes/__root.tsx
+++ packages/web/src/routes/__root.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useRef } from "react"
 import type { QueryClient } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router"
-import { TanStackRouterDevtools } from "@tanstack/router-devtools"
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools"
 import { DesktopTitlebar } from "@/components/layout/desktop-titlebar"
 import { language } from "@/context/language"
 import { usePlatform } from "@/context/platform"
@@ -81,7 +82,12 @@ function RootLayout() {
       <div className="min-h-0 flex-1">
         <Outlet />
       </div>
-      {import.meta.env.DEV && <TanStackRouterDevtools position="bottom-right" />}
+      {import.meta.env.DEV && (
+        <>
+          <TanStackRouterDevtools position="bottom-right" />
+          <ReactQueryDevtools buttonPosition="bottom-left" />
+        </>
+      )}
     </div>
   )
 }
