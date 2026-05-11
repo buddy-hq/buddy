625ef4094097ca25b7e5aaaac4fd989b4106b6bd
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2026-05-11 17:02:21 +0530

    fix(question-sets): restrict save_question_set tool to subagent only

--- packages/buddy/src/learning/features/question-sets/feature.ts
+++ packages/buddy/src/learning/features/question-sets/feature.ts
@@ -1,10 +1,9 @@
 import { defineBuddyFeature } from "../../runtime/define-buddy-feature"
 import { QUESTION_SET_AUTHOR_AGENT } from "./subagents/question-set-author"
-import { saveQuestionSetTool } from "./tools/save-question-set"
 
 export const questionSetsFeature = defineBuddyFeature({
   id: "question-sets",
-  tools: [saveQuestionSetTool],
+  tools: [],
   skills: [],
   subagents: [QUESTION_SET_AUTHOR_AGENT],
   surfaces: ["question-set"],
