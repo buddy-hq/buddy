253ae9d5285b34734e8b549d77b496592b56502e
Prashant Bhudwal <Prashant.Bhudwal@gmail.com>
2026-05-15 13:04:07 +0530

    fix(figure-rendering): Rename export and id to figureRenderingFeature

--- packages/buddy/src/learning/features/figure-rendering/feature.ts
+++ packages/buddy/src/learning/features/figure-rendering/feature.ts
@@ -2,8 +2,8 @@ import { defineBuddyFeature } from "../../runtime/define-buddy-feature"
 import { renderFigureTool } from "./geometry/tools/render-figure"
 import { renderFreeformFigureTool } from "./freeform/tools/render-freeform-figure"
 
-export const mathFiguresFeature = defineBuddyFeature({
-  id: "math-figures",
+export const figureRenderingFeature = defineBuddyFeature({
+  id: "figure-rendering",
   tools: [renderFigureTool, renderFreeformFigureTool],
   skills: [],
   subagents: [],
